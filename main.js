9dcf9f795a457036af0b78df75925b9a956174d8
Amir Ismayilov <amir@Amirs-MacBook-Air.local>
2024-02-23 23:46:43 +0400

    Add animation.

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.animate-on-scroll').each(function () {
+            var elementTop = $(this).offset().top;
+            var elementBottom = elementTop + $(this).outerHeight();
+            var viewportTop = $(window).scrollTop();
+            var viewportBottom = viewportTop + $(window).height();
+
+            if (elementBottom > viewportTop && elementTop < viewportBottom) {
+                $(this).addClass($(this).data('animation')).css('visibility', 'visible');
+            } else {
+                $(this).removeClass($(this).data('animation')).css('visibility', 'hidden');
+            }
+        });
+    }).scroll(); // Trigger scroll event on page load
+});
+
 // QR animation
 var square = document.querySelector('.square-wrapper');
 
